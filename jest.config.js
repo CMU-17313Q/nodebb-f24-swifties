5daa3b1a4414cc4050ab11aaaf46994abfa24bff
NickDevi <ndevidze@andrew.cmu.edu>
2024-10-20 23:38:54 +0300

    added the tests file and the jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    roots: ['<rootDir>/test'],  // Set the root directory to the test folder
+    moduleDirectories: ['node_modules', '<rootDir>'],
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: ['**/test/mocks/plugin_modules/@nodebb/**/*.js'],  // Specify where to find test files
+  };
